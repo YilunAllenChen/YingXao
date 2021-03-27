1076e7318bb11dd9863d33f17a4de86f10cbcfb7
savage12138 <yida12138@gmail.com>
2021-03-27 18:56:42 -0400

    move old DHT22 lib to archive

--- YingXao_Embedded/sensor_dev/dht22.py
+++ YingXao_Embedded/sensor_dev/dht22.py
@@ -1,49 +1,22 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
+import Adafruit_DHT
 
-import time
-import board
-import adafruit_dht
-
-# Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT22(board.D21)
-
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 16
 
 def get_temperature_c():
-    return dhtDevice.temperature
+    return round(Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)[1],1)
 
 def get_temperature_f():
-    return round((dhtDevice.temperature) * (9 / 5) + 32, 1)
+    return round(get_temperature_c() * (9 / 5) + 32,1)
 
 def get_humidity():
-    return dhtDevice.humidity
+    return round(Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)[0],1)
 
-if __name__=='__main__':
+if __name__ == "__main__":
     while True:
-        try:
-            # Print the values to the serial port
-            # temperature_c = dhtDevice.temperature
-            # temperature_f = temperature_c * (9 / 5) + 32
-            # humidity = dhtDevice.humidity
-            print(
-                "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                    get_temperature_f(), get_temperature_c(), get_humidity()
-                )
-            )
-
-        except RuntimeError as error:
-            # Errors happen fairly often, DHT's are hard to read, just keep going
-            print(error.args[0])
-            time.sleep(2.0)
-            continue
-        except Exception as error:
-            print(error)
-            dhtDevice.exit()
-            raise error
-
-        time.sleep(2.0)
-
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        if humidity is not None and temperature is not None:
+            print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+            print("Temp in C = {}, F = {}, Humidity = {}".format(get_temperature_c(),get_temperature_f(),get_humidity()))
+        else:
+            print("Failed to retrieve data from humidity sensor")
